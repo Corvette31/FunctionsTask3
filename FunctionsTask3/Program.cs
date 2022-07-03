0ebf0294c587be536a8f41cf7a4db07d458655a3
Corvette31 <corvette.ss@yandex.ru>
2022-07-04 00:20:33 +0300

    Правки

--- FunctionsTask3/Program.cs
+++ FunctionsTask3/Program.cs
@@ -6,21 +6,28 @@ namespace FunctionsTask3
     {
         static void Main(string[] args)
         {
+            bool isConverted = false;
             int number = 0;
 
-            while (ConvertToNumber(ref number) == false)
+            while (isConverted == false)
             {
-                Console.WriteLine("Ошибка конвертации!");
+                Console.WriteLine("Введите число");
+                number = ConvertToNumber(ref isConverted);
+                if (isConverted == false)
+                {
+                    Console.WriteLine("Ошибка конвертации!");
+                }
             }
 
             Console.WriteLine($"Число {number} успешно было сконвертированно из строки");
         }
 
-        static bool ConvertToNumber(ref int number)
+        static int ConvertToNumber(ref bool isConverted)
         {
-            Console.WriteLine("Введите число");
+            int number;
+            isConverted = int.TryParse(Console.ReadLine(), out number);
 
-            return int.TryParse(Console.ReadLine(), out number);
+            return number;
         }
     }
 }
