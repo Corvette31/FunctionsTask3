70407134273e155ac04d157b91289f9db21e1d8a
Corvette31 <corvette.ss@yandex.ru>
2022-07-03 23:59:44 +0300

    Добавьте файлы проекта.

---
+++ FunctionsTask3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FunctionsTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            while (ConvertToNumber(ref number) == false)
+            {
+                Console.WriteLine("Ошибка конвертации!");
+            }
+
+            Console.WriteLine($"Число {number} успешно было сконвертированно из строки");
+        }
+
+        static bool ConvertToNumber(ref int number)
+        {
+            Console.WriteLine("Введите число");
+
+            return int.TryParse(Console.ReadLine(), out number);
+        }
+    }
+}
